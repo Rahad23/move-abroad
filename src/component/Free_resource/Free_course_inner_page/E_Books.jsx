759e558e9b0389954bee264412e46cd37ec19eaf
Rahad23 <rahadhasan33675@gmail.com>
2023-07-08 16:31:11 +0600

    solve responsive problem

--- src/component/Free_resource/Free_course_inner_page/E_Books.jsx
+++ src/component/Free_resource/Free_course_inner_page/E_Books.jsx
@@ -28,19 +28,19 @@ const E_Books=()=>{
         <div className="grid lg:grid-cols-3 grid-cols-1 mx-auto gap-y-7 my-14">
             {
                 bookData.map(data=>
-                    <div className="card w-[350px] rounded-none bg-white shadow-xl mx-auto cursor-pointer" key={data.id}>
+                    <div className="card w-[350px] rounded-none bg-white shadow-xl mx-auto cursor-pointer" key={data._id}>
                    <div className="container">
                    <figure><img className="image" src={cardImg} alt="Shoes" /></figure>
                     <div className="middle w-full">
-                        <h1 className="text text-xl font-semibold">{data?.hoverText}</h1>
+                        <h1 className="text text-xl font-semibold">{data?.bookName}</h1>
                     </div>
                    </div>
                     <div className="card-body px-3 py-6">
-                      <h2 className="card-title text-gray-950 text-lg leading-7 font-serif" title={data.title}>
-                       {data.title.length >= 60 ? data.title.slice(0, 59)+"..." : data.title}
+                      <h2 className="card-title text-gray-950 text-lg leading-7 font-serif" title={data.description}>
+                       {data.description.length >= 60 ? data.description.slice(0, 59)+"..." : data.description}
                       </h2>
                       <div className="card-actions justify-end mt-2">
-                        <Link to={`${data.id}`}>
+                        <Link to={`${data._id}`}>
                         <button className="bg-[#ffffff] text-[13px] border-[2px] border-[#D82027] hover:bg-[#D82027] hover:text-white text-sm rounded-none w- h-[36px] w-[103px] uppercase">Know more</button>
                         </Link>
                       </div>
